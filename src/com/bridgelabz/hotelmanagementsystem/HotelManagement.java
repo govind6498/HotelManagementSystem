d62b633d3e36c6d3344507cedb4269cb901db303
govind6498 <govindsingh6498@gmail.com>
2021-09-16 08:20:10 +0530

    Added:To Display Welcome to Hotel Management System

---
+++ src/com/bridgelabz/hotelmanagementsystem/HotelManagement.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.hotelmanagementsystem;
+
+public class HotelManagement {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Management System");
+	}
+}
