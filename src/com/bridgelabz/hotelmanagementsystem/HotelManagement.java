9de7dd6226527280e47d587572630e5053c8173f
govind6498 <govindsingh6498@gmail.com>
2021-09-16 08:44:01 +0530

    hotel_management_system_use_case_1

--- src/com/bridgelabz/hotelmanagementsystem/HotelManagement.java
+++ src/com/bridgelabz/hotelmanagementsystem/HotelManagement.java
@@ -1,6 +1,22 @@
 package com.bridgelabz.hotelmanagementsystem;
 
 public class HotelManagement {
+	private String nameForRegularCustomer;
+	private double rateForRegularCustomer;
+	
+	public String getNameForRegularCustomer() {
+		return nameForRegularCustomer;
+	}
+	public void setNameForRegularCustomer(String nameForRegularCustomer) {
+		this.nameForRegularCustomer=nameForRegularCustomer;
+	}
+	public double getRateForRegularCustomer() {
+		return rateForRegularCustomer;
+	}
+	public void setRateForRegularCustomer(double rateForRegularCustomer) {
+		this.rateForRegularCustomer=rateForRegularCustomer;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Hotel Management System");
 	}
